befc4a0df94e53091305d773d1af9db07d039ef0
TadejGrof <49908682+TadejGrof@users.noreply.github.com>
2019-08-28 23:14:33 +0200

    Poboljšan spletni vmesnik

--- Spletni_vmesnik.py
+++ Spletni_vmesnik.py
@@ -5,6 +5,7 @@ serija = None
 oznacena_domina = None
 gledana_igra = None
 
+#######################################################################################################
 @bottle.route('/naslovna_stran/design/<filepath:path>')
 def server_static1(filepath):
     return bottle.static_file(filepath, root = "./static/")
@@ -13,36 +14,27 @@ def server_static1(filepath):
 def server_static2(filepath):
     return bottle.static_file(filepath, root = "./static/")
 
-@bottle.route('/konec_serije/design/<filepath:path>')
+@bottle.route('/konec_partije/design/<filepath:path>')
 def server_static3(filepath):
     return bottle.static_file(filepath, root = "./static/")
 
+@bottle.route('/konec_partije/ogled_partije/design/<filepath:path>')
+def server_static4(filepath):
+    return bottle.static_file(filepath, root = "./static/")
 
-@bottle.get('/klik_domine/<stevilka>')
-def klik_domine(stevilka):
-    global oznacena_domina
-    oznacena_domina = int(stevilka)
-    bottle.redirect("/domino/")
+@bottle.route('/konec_serije/design/<filepath:path>')
+def server_static5(filepath):
+    return bottle.static_file(filepath, root = "./static/")
 
-@bottle.get('/ogled_partije/<index>')
-def ogled_partije(index):
-    global serija
-    global gledana_igra
-    gledana_igra = serija.koncane_igre[-int(index)]
-    bottle.redirect('/domino/')
+@bottle.route('/konec_serije/ogled_partije/design/<filepath:path>')
+def server_static6(filepath):
+    return bottle.static_file(filepath, root = "./static/")
 
-@bottle.post('/serija_reset/')
-def serija_reset():
-    global serija
-    global gledana_igra
-    gledana_igra = None
-    serija = None
-    bottle.redirect('/')
+#########################################################################################################
 
 @bottle.get('/')
 def redirect():
     bottle.redirect('/naslovna_stran/')
-
 
 @bottle.get('/naslovna_stran/')
 def osnovna_stran():
@@ -56,11 +48,76 @@ def osnovna_stran():
 def nova_serija():
     global serija
     ime = bottle.request.forms["ime"]
-    serija = Serija_Iger(Igralec(ime),Igralec(),30)
-    bottle.redirect("/nova_partija/")
-        
+    serija = Serija_Iger(Igralec(ime),Igralec(),100)
+    serija.nova_igra()
+    igra = serija.aktivna_igra
+    igra.doloci_design()
+    igra.razdeli()
+    bottle.redirect("/domino/")
 
-@bottle.post('/igralec_poteza/<stran>')
+@bottle.post("/nova_partija/")
+def nova_partija():
+    global serija
+    serija.nova_igra()
+    igra = serija.aktivna_igra
+    igra.doloci_design()
+    igra.razdeli()
+    if len(serija.koncane_igre)> 0:
+        igra.doloci_igralca_na_potezi(serija.koncane_igre[-1].zmagovalec)
+    bottle.redirect("/domino/")
+
+###################################################################################################
+
+@bottle.get('/domino/')
+def domino():
+    global serija
+    global oznacena_domina
+    global gledana_igra
+    if gledana_igra == None:
+        igra = serija.aktivna_igra
+    else:
+        igra = gledana_igra
+    return bottle.template('domino.tpl',serija = serija, igra=igra, index = oznacena_domina)
+
+@bottle.get('/domino/klik_domine/<stevilka>')
+def klik_domine(stevilka):
+    global oznacena_domina
+    oznacena_domina = int(stevilka)
+    bottle.redirect("/domino/")
+
+@bottle.post('/domino/zacetna_poteza/')
+def zacetna_poteza():
+    global serija
+    igra = serija.aktivna_igra
+    zacetna_poteza_prve_igre(igra)
+    bottle.redirect("/domino/")
+
+@bottle.post('/domino/racunalnikova_poteza/')
+def poteza2():
+    global serija
+    igra = serija.aktivna_igra
+    if len(igra.poteze) == 0:
+        prva_poteza(igra)
+    else:
+        poteza(igra)
+    bottle.redirect("/domino/")
+
+@bottle.post('/domino/igralec_poteza/dodaj')
+def poteza_igralec_dodaj():
+    global serija
+    igra = serija.aktivna_igra
+    nakljucna = igra.nakljucna_iz_nerazdeljenih()
+    igra.igralec1.dodaj_domino(nakljucna)
+    bottle.redirect('/domino/')
+
+@bottle.post('/domino/igralec_poteza/potrkaj')
+def poteza_igralec_potrkaj():
+    global serija
+    igra = serija.aktivna_igra
+    igra.naslednji_na_potezi()
+    bottle.redirect('/domino/')
+
+@bottle.post('/domino/igralec_poteza/<stran>')
 def poteza_igralec(stran):
     global serija
     global oznacena_domina
@@ -72,49 +129,32 @@ def poteza_igralec(stran):
     oznacena_domina = None
     bottle.redirect("/domino/")
 
-@bottle.get("/nova_partija/")
-def nova_partija():
-    global serija
-    serija.nova_igra()
-    igra = serija.aktivna_igra
-    igra.doloci_design()
-    igra.razdeli()
-    bottle.redirect("/domino/")
-
-@bottle.get('/domino/')
-def domino():
-    global serija
-    global oznacena_domina
-    global gledana_igra
-    if gledana_igra == None:
-        igra = serija.aktivna_igra
-    else:
-        igra = gledana_igra
-    return bottle.template('domino.tpl',serija = serija,igra=igra, index = oznacena_domina)
-
-@bottle.post('/domino/poteza/')
-def poteza2():
-    global serija
-    igra = serija.aktivna_igra
-    if len(igra.poteze) == 0:
-        zacetna_poteza(igra)
-        bottle.redirect("/domino/")
-    elif len(igra.poteze)>0:
-        if not igra.je_konec_igre():
-            poteza(igra)
-            bottle.redirect("/domino/")
-        else:
-            konec_partije(serija)
-
-def konec_partije(serija):
+@bottle.post('/domino/konec_partije/')
+def konec_parije2():
     igra = serija.aktivna_igra
     igra.konec_igre(serija)
     igra.zmagovalec.tocke_po_zmagi(igra.koncne_tocke)
-
     if not serija.je_konec():
-        bottle.redirect("/domino/konec_partije/")
+        bottle.redirect("/konec_partije/")
     else:
         bottle.redirect("/konec_serije/")
+
+###########################################################################################################
+
+@bottle.get("/konec_partije/")
+def konec_igre():
+    global serija
+    igra = serija.koncane_igre[-1]
+    return bottle.template('konec_partije.tpl', igra = igra, serija = serija)
+
+@bottle.get("/konec_partije/ogled_partije/")
+def konec_partije_ogled_partije():
+    global serija
+    global oznacena_domina
+    igra = serija.koncane_igre[-1]
+    return bottle.template("domino.tpl",serija = serija, igra = igra, index = oznacena_domina)
+
+
 
 @bottle.get("/konec_serije/")
 def konec_serije():
@@ -122,47 +162,64 @@ def konec_serije():
     serija.vrni_zmagovalca()
     return bottle.template('konec_serije.tpl',serija = serija)
 
-@bottle.get("/domino/konec_partije/")
-def konec_igre():
+@bottle.get('/konec_serije/ogled_partije/<index>')
+def ogled_partije(index):
     global serija
-    igra = serija.koncane_igre[-1]
-    return bottle.template('konec_partije.tpl',igra=igra)
+    global gledana_igra
+    global oznacena_domina
+    igra = gledana_igra
+    return bottle.template("domino.tpl",serija = serija,igra = igra, index = index)
 
-def zacetna_poteza(igra):
+@bottle.post('/konec_serije/serija_reset/')
+def serija_reset():
+    global serija
+    global gledana_igra
+    gledana_igra = None
+    serija = None
+    bottle.redirect('/')
+
+##############################################################################################################
+##############################################################################################################
+##############################################################################################################
+
+def zacetna_poteza_prve_igre(igra):
     poteza = igra.zacetna_poteza()
     igra.doloci_igralca_na_potezi(poteza.igralec)
     igra.poteza(poteza)
     igra.zadnja_poteza().doloci_design(igra)
 
-def dodaj_domino(igra):
-    nakljucna = igra.nakljucna_iz_nerazdeljenih()
-    igra.igralec_na_potezi.dodaj_domino(nakljucna)
-    besedilo = igra.igralec_na_potezi.ime + ' doda ' + str(nakljucna)
-    return besedilo
-
-def brez_poteze(igra):
-    besedilo = igra.igralec_na_potezi.ime + ' ne more narediti poteze in ne more dodati domine. <br> Na potezi je: ' + str(igra.igralec_na_potezi.nasprotnik(igra))
-    igra.naslednji_na_potezi()
-    return besedilo
-
-def igralceva_poteza(igra):
-    poteza = igra.nakljucna_poteza()
-    poteza.doloci_design(igra)
-    if poteza.stran == "L":
-        besedilo = poteza.igralec.ime + ' igra ' + str(poteza.domino) + ' na levo stran polja.'
-    elif poteza.stran == "D":
-        besedilo = poteza.igralec.ime + ' igra ' + str(poteza.domino) + ' na desno stran polja.'
-    return besedilo
+def prva_poteza(igra):
+    poteza = igra.igralec_na_potezi.premisljena_prva_poteza()
+    igra.poteza(poteza)
+    igra.zadnja_poteza().doloci_design(igra)
 
 def poteza(igra):
     mozne_poteze = igra.igralec_na_potezi.stevilo_moznih_potez(igra.igrane_domine)
     if mozne_poteze == 0:
         if len(igra.nerazdeljene_domine) > 0:
-            besedilo = dodaj_domino(igra)
+            dodaj_domino(igra)
         elif len(igra.nerazdeljene_domine) == 0:
-            besedilo = brez_poteze(igra)
+            brez_poteze(igra)
     elif mozne_poteze > 0:
-        besedilo = igralceva_poteza(igra)
-    return besedilo
+        igralceva_poteza(igra)
+    
+def igralceva_poteza(igra):
+    if len(igra.poteze) <= 8:
+        poteza = igra.igralec_na_potezi.zgodnja_premisljena_poteza(igra.igrane_domine)
+        igra.poteza(poteza)
+    else:
+        poteza = igra.igralec_na_potezi.pozna_premisljena_poteza(igra.igrane_domine,igra)
+        igra.poteza(poteza)
+    poteza.doloci_design(igra)
+
+def dodaj_domino(igra):
+    nakljucna = igra.nakljucna_iz_nerazdeljenih()
+    igra.igralec_na_potezi.dodaj_domino(nakljucna)
+
+def brez_poteze(igra):
+    igra.naslednji_na_potezi()
+    
+
+################################################################################################################
 
 bottle.run(debug=True, reloader=True)
